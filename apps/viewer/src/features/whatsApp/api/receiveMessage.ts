b087ad456bd1c598f547873aef4cb71e329ee80e
Wigberto Oliveira <52203487+wigbertojr@users.noreply.github.com>
2023-09-11 19:03:12 -0300

    Update receiveMessage.ts

--- apps/viewer/src/features/whatsApp/api/receiveMessage.ts
+++ apps/viewer/src/features/whatsApp/api/receiveMessage.ts
@@ -1,32 +1,28 @@
 import { publicProcedure } from '@/helpers/server/trpc'
 import { whatsAppWebhookRequestBodySchema } from '@typebot.io/schemas/features/whatsapp'
-import { z } from 'zod'
 import { resumeWhatsAppFlow } from '../helpers/resumeWhatsAppFlow'
+import { z } from 'zod'
 import { isNotDefined } from '@typebot.io/lib'
-import { TRPCError } from '@trpc/server'
-import { env } from '@typebot.io/env'
 
-export const receiveMessagePreview = publicProcedure
+export const receiveMessage = publicProcedure
   .meta({
     openapi: {
       method: 'POST',
-      path: '/whatsapp/preview/webhook',
-      summary: 'WhatsApp',
+      path: '/workspaces/{workspaceId}/whatsapp/phoneNumbers/{phoneNumberId}/webhook',
+      summary: 'Receive WhatsApp Message',
     },
   })
-  .input(whatsAppWebhookRequestBodySchema)
+  .input(
+    z
+      .object({ workspaceId: z.string(), phoneNumberId: z.string() })
+      .merge(whatsAppWebhookRequestBodySchema)
+  )
   .output(
     z.object({
       message: z.string(),
     })
   )
-  .mutation(async ({ input: { entry } }) => {
-    if (!env.WHATSAPP_PREVIEW_FROM_PHONE_NUMBER_ID)
-      throw new TRPCError({
-        code: 'INTERNAL_SERVER_ERROR',
-        message: 'WHATSAPP_PREVIEW_FROM_PHONE_NUMBER_ID is not defined',
-      })
-
+  .mutation(async ({ input: { entry, workspaceId, phoneNumberId } }) => {
     const receivedMessage = entry.at(0)?.changes.at(0)?.value.messages?.at(0)
     if (isNotDefined(receivedMessage)) return { message: 'No message found' }
 
@@ -44,8 +40,9 @@ export const receiveMessagePreview = publicProcedure
 
     return resumeWhatsAppFlow({
       receivedMessage,
-      sessionId: `wa-${receivedMessage.from}-preview`,
-      phoneNumberId: env.WHATSAPP_PREVIEW_FROM_PHONE_NUMBER_ID,
+      sessionId: `wa-${phoneNumberId}-${receivedMessage.from}`,
+      phoneNumberId,
+      workspaceId,
       contact: {
         name: contactName,
         phoneNumber: contactWaId, // Use wa_id instead of display_phone_number
